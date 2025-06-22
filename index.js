04dd9d41a6a39995c47f33d6b4ee46cdb88a1f5d
abhishekbapna51 <abhishekbapna51@gmail.com>
2025-06-22 14:36:57 +0530

    Add start script for Render deployment

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/tasks", require("./routes/taskRoutes"));
-
-
 app.use("/api/auth", require("./routes/authRoutes"));
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.log(err));
